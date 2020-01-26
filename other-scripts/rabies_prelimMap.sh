5ea72eb7fa088093f3c229ee7f93f661c3d8e5e4
Kirstyn Brunker <kirstyn.brunker@glasgow.ac.uk>
2020-01-26 17:31:46 +0000

    Fixes to prelim map script

--- other-scripts/rabies_prelimMap.sh
+++ other-scripts/rabies_prelimMap.sh
@@ -17,18 +17,18 @@ minimap2 -ax map-ont ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.f
 align_trim --normalise 200 ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $runname"_"$bc"_prelim".alignreport.txt < $runname"_"$bc"_prelim".bam 2> $runname"_"$bc"_prelim".alignreport.er | samtools view -bS - | samtools sort -T temp - -o $runname"_"$bc"_prelim".primertrimmed.sorted.bam
 
 #produce quick consensus
-bcftools mpileup -Ou -f ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta $runname"_"$bc"_prelim".primertrimmed.sorted.bam | bcftools call -mv --skip-variants indels --ploidy 1 -Oz -o $runname"_"$bc"_prelim".vcf.gz 
+bcftools mpileup -Ou -f ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta $runname"_"$bc"_prelim".primertrimmed.sorted.bam | bcftools call -mv --skip-variants indels --ploidy 1 -Oz -o $runname"_"$bc"_prelim".vcf.gz
 tabix $runname"_"$bc"_prelim".vcf.gz
 cat ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta | bcftools consensus $runname"_"$bc"_prelim".vcf.gz > $runname"_prelim".reference.fasta
 
-#rename files/headers to create new scheme based on prelim reference 
+#rename files/headers to create new scheme based on prelim reference
 #fasta:
 awk '/^>/ {gsub(/.fa(sta)?$/,"",FILENAME);printf(">%s\n",FILENAME);next;} {print}' $runname"_prelim".reference.fasta > $output/$runname"_prelim".reference.fasta
 samtools faidx $output/$runname"_prelim".reference.fasta
 bwa index $output/$runname"_prelim".reference.fasta
 
-#bed:
+#create prelim bed:
 newname=$(grep -e ">" $output/$runname"_prelim".reference.fasta | awk 'sub(/^>/, "")')
+echo $newname
 cp ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed $output/$runname"_prelim".scheme.bed
-sed -i "s/\(KX148266|AB517659\)/${newname}/g" $output/$runname"_prelim".scheme.bed
-
+sed -i -e "s/AB517659/${newname}/g" -e "s/KF155002.1/${newname}/g" -e "s/KX148275/${newname}/g" $output/$runname"_prelim".scheme.bed
