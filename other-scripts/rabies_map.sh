be7848846a68904a273e92fd5c6e82b73ca50fe4
Kirstyn Brunker <kirstyn.brunker@glasgow.ac.uk>
2020-01-26 20:22:18 +0000

    Fixes to scripts: - prelim mapping of reads - mapping and primer trimming

--- other-scripts/rabies_map.sh
+++ other-scripts/rabies_map.sh
@@ -6,7 +6,7 @@ set -e
 runname=$1
 pathToFastq=$2
 ref=$3
-files=${pathToFastq}/${runname}_all-NB01.fastq
+files=${pathToFastq}/${runname}_all-NB02.fastq
 echo $files
 
 echo -e "sample\ttime\treads\tmapped\tbasesCovered\tbasesCoveredx20\tbasesCoveredx100" > $runname"_nb_mappingstats".txt
@@ -15,7 +15,10 @@ for f in $files
 do
 #start=`date +%s`
 nb=$(echo "$f" | sed 's/.*\(NB[0-9][0-9]\).*/\1/')
+mkdir -p ~/pipeline_output/primer-schemes/$runname"_prelim"/$nb
+output=$(echo ~/pipeline_output/primer-schemes/$runname"_prelim"/$nb)
 mkdir -p ~/pipeline_output/minion_output/$runname/$nb
+output2=$(echo ~/pipeline_output/minion_output/$runname/$nb)
 
 ~/github/artic-rabv/other-scripts/rabies_prelimMap.sh $ref $pathToFastq $runname $nb
 
@@ -23,34 +26,29 @@ mkdir -p ~/pipeline_output/minion_output/$runname/$nb
 #artic minion --normalise 200 --threads 4 --scheme-directory '~/pipeline_output/primer-schemes' --read-file $pathToFastq/$runname"_all-"$nb.fastq --nanopolish-read-file $pathToFastq/$runname"_all.fastq" $runname"_prelim"/$nb $runname"_"$nb"_prelimMapped"
 
 #produce bam file for primer trimming step
-#minimap2 -ax map-ont /home2/kb157b/pipeline_output/primer-schemes $runname"_all-"$bc.fastq | samtools view -u -| samtools sort - -T temp -o $output/$bc".sorted.bam"
+minimap2 -ax map-ont $output/$runname"_prelim".reference.fasta $f | samtools view -u -| samtools sort - -T temp -o $output2/$runname"_"$nb".sorted.bam"
 
 #primer trimming
-#align_trim  --normalise 200 ~/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $output/$bc".sorted".alignreport.txt < $output/$bc".sorted.bam" 2> $output/$bc".sorted".alignreport.er | samtools view -bS - | samtools sort -T %s - -o $output/$bc".primertrimmed.sorted.bam" 
-#samtools index $output/$bc".primertrimmed.sorted.bam" 
+align_trim  --normalise 200 ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $output2/$runname"_"$nb".sorted".alignreport.txt < $output2/$runname"_"$nb".sorted.bam" 2> $output2/$nb".sorted".alignreport.er | samtools view -bS - | samtools sort -T %s - -o $output2/$runname"_"$nb".primertrimmed.sorted.bam"
+samtools index $output2/$runname"_"$nb".primertrimmed.sorted.bam"
 
 #convert trimmed bam file to fastq for medaka step
-#samtools bam2fq $output/$bc".primertrimmed.sorted.bam" > $output/$bc".primertrimmed.sorted".fastq
+samtools bam2fq $output2/$runname"_"$nb".primertrimmed.sorted.bam" > $output2/$runname"_"$nb".primertrimmed.sorted".fastq
 
 #produce consensus in medaka
-#medaka_consensus -i $output/$bc".primertrimmed.sorted".fastq -d $output/$bc"_racon4".fasta -o $output/$bc"_medaka" -t 2 -m r941_min_fast_g303
+#medaka_consensus -i $output/$nb".primertrimmed.sorted".fastq -d $output/$nb"_racon4".fasta -o $output/$nb"_medaka" -t 2 -m r941_min_fast_g303
 #end=`date +%s`
 #runtime=$((end-start))
 
 #summary mapping stats
-reads=$(samtools view $runname"_"$nb"_prelimMapped.sorted.bam" | cut -f 1 | sort | uniq | wc -l) 
-mapped=$(samtools view -F 4 $runname"_"$nb"_prelimMapped.sorted.bam" | cut -f 1 | sort | uniq | wc -l) 
-basesCovered=$(samtools depth $runname"_"$nb"_prelimMapped.trimmed.sorted.bam" | awk '($3>0)' |wc -l)
-basesCoveredx20=$(samtools depth $runname"_"$nb"_prelimMapped.trimmed.sorted.bam" | awk '($3>=20)' |wc -l) 
-basesCoveredx100=$(samtools depth $runname"_"$nb"_prelimMapped.trimmed.sorted.bam" | awk '($3>=100)' |wc -l) 
+reads=$(samtools view $output2/$runname"_"$nb".sorted.bam" | cut -f 1 | sort | uniq | wc -l)
+mapped=$(samtools view -F 4 $output2/$runname"_"$nb".sorted.bam" | cut -f 1 | sort | uniq | wc -l)
+basesCovered=$(samtools depth $output2/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>0)' |wc -l)
+basesCoveredx20=$(samtools depth $output2/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>=20)' |wc -l)
+basesCoveredx100=$(samtools depth $output2/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>=100)' |wc -l)
 echo -e $runname"_"${nb}"\t"$runtime"\t"$reads"\t"$mapped"\t"$basesCovered"\t"$basesCoveredx20"\t"$basesCoveredx100 >>$runname"_nb_mappingstats".txt
 
-
-#organise output files
-mv $runname"_"$nb"_prelimMapped"* ~/pipeline_output/minion_output/$runname/$nb
-
-done
+ done
 mkdir -p ~/pipeline_output/archived
 mv $runname"_nb_mappingstats".txt ~/pipeline_output/minion_output/$runname
 mv $runname* ~/pipeline_output/archived
-
--- other-scripts/rabies_prelimMap.sh
+++ other-scripts/rabies_prelimMap.sh
@@ -5,21 +5,19 @@ set -e
 ref=$1
 pathToFastq=$2
 runname=$3
-bc=$4
-
-#make directories for storage only if they don't exist already
-mkdir -p ~/pipeline_output/primer-schemes/$runname"_prelim"/$bc
-output=$(echo ~/pipeline_output/primer-schemes/$runname"_prelim"/$bc)
+nb=$4
+mkdir -p ~/pipeline_output/primer-schemes/$runname"_prelim"/$nb
+output=$(echo ~/pipeline_output/primer-schemes/$runname"_prelim"/$nb)
 #preliminary map to original reference
-minimap2 -ax map-ont ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta $pathToFastq/$runname"_all-"$bc.fastq | samtools view -u -| samtools sort - -T temp -o $runname"_"$bc"_prelim".bam
+minimap2 -ax map-ont ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta $pathToFastq/$runname"_all-"$nb.fastq | samtools view -u -| samtools sort - -T temp -o $runname"_"$nb"_prelim".bam
 
 #trim primers and normalise coverage for better representation
-align_trim --normalise 200 ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $runname"_"$bc"_prelim".alignreport.txt < $runname"_"$bc"_prelim".bam 2> $runname"_"$bc"_prelim".alignreport.er | samtools view -bS - | samtools sort -T temp - -o $runname"_"$bc"_prelim".primertrimmed.sorted.bam
+align_trim --normalise 200 ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $runname"_"$nb"_prelim".alignreport.txt < $runname"_"$nb"_prelim".bam 2> $runname"_"$nb"_prelim".alignreport.er | samtools view -bS - | samtools sort -T temp - -o $runname"_"$nb"_prelim".primertrimmed.sorted.bam
 
 #produce quick consensus
-bcftools mpileup -Ou -f ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta $runname"_"$bc"_prelim".primertrimmed.sorted.bam | bcftools call -mv --skip-variants indels --ploidy 1 -Oz -o $runname"_"$bc"_prelim".vcf.gz
-tabix $runname"_"$bc"_prelim".vcf.gz
-cat ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta | bcftools consensus $runname"_"$bc"_prelim".vcf.gz > $runname"_prelim".reference.fasta
+bcftools mpileup -Ou -f ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta $runname"_"$nb"_prelim".primertrimmed.sorted.bam | bcftools call -mv --skip-variants indels --ploidy 1 -Oz -o $runname"_"$nb"_prelim".vcf.gz
+tabix $runname"_"$nb"_prelim".vcf.gz
+cat ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta | bcftools consensus $runname"_"$nb"_prelim".vcf.gz > $runname"_prelim".reference.fasta
 
 #rename files/headers to create new scheme based on prelim reference
 #fasta:
