f8037ee38f6d435c4073f9535b6e7530a9f29092
Kathryn Campbell <2497997C@student.gla.ac.uk>
2020-01-29 17:47:26 +0000

    Script updates: -  process from compressed fast5 file - compatible with latest artic scripts

--- other-scripts/rabies_processRaw.sh
+++ other-scripts/rabies_processRaw.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 set -e
 
-rawFast5=$1
+rawFast5file=$1
 runname=$2
+filepath=$(dirname $rawFast5file)
+rawFast5=${rawFast5file%.tar.gz}
+
+#extract compressed reads
+tar -xvzf $rawFast5file -C $filepath
 
 #basecall raw fast5 files
-guppy_basecaller -r --input_path --require_barcodes_both_ends $rawFast5 --save_path "guppy_"$runname -c dna_r9.4.1_450bps_fast.cfg
+guppy_basecaller -r --input_path $rawFast5 --save_path "basecalled_"$runname -c dna_r9.4.1_450bps_fast.cfg --device cuda:0
 
 # gather fastq files
-artic gather --guppy --min-length 300 $runname "guppy_"$runname
+artic gather --min-length 300 --directory "basecalled_"$runname --prefix $runname
 
 #demultiplex with porechop
 artic demultiplex --threads 4 $runname"_all.fastq"
 
 #index raw reads
-nanopolish index -d $rawFast5 $runname"_all.fastq"disku
+nanopolish index -d $rawFast5 $runname"_all.fastq"
