c7525e23d153f8d2c3ca4807054bdda60704db4f
Kirstyn Brunker <kirstyn.brunker@glasgow.ac.uk>
2020-01-31 14:18:36 +0000

    Corrections to produce trimmed fastq file

--- other-scripts/rabies_map.sh
+++ other-scripts/rabies_map.sh
@@ -18,40 +18,33 @@ do
 nb=$(echo "$f" | sed 's/.*\(NB[0-9][0-9]\).*/\1/')
 
 #setup output files (newscheme and prelim already made in prelim script but need to set up as variables here)
-newscheme=$(echo ~/pipeline_output/$runname/prelim/primer-schemes/$nb)
+newscheme=$(echo ~/pipeline_output/primer-schemes/$runname"_prelim"/$nb)
 prelim=$(echo ~/pipeline_output/$runname/prelim/mapped/$nb)
 #initial mapping and primer trim
 mkdir -p ~/pipeline_output/$runname/initial_mapTrim/$nb
 trim=$(echo ~/pipeline_output/$runname/initial_mapTrim/$nb)
+#
+# #run preliminary mapping step to produce new reference scheme
+# ~/github/artic-rabv/other-scripts/rabies_prelimMap.sh $ref $pathToFastq $runname $nb
+#
+# #produce bam file for primer trimming step
+# minimap2 -ax map-ont $newscheme/$runname"_prelim".reference.fasta $f | samtools view -u -| samtools sort - -T temp -o $trim/$runname"_"$nb".sorted.bam"
+# # #primer trimming
+# align_trim  --normalise 200 ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $trim/$runname"_"$nb".sorted".alignreport.txt < $trim/$runname"_"$nb".sorted.bam" 2> $trim/$nb".sorted".alignreport.er | samtools view -bS - | samtools sort -T %s - -o $trim/$runname"_"$nb".primertrimmed.sorted.bam"
+# samtools index $trim/$runname"_"$nb".primertrimmed.sorted.bam"
+# align_trim  ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $trim/$runname"_"$nb".sorted".alignreport.txt < $trim/$runname"_"$nb".sorted.bam" 2> $trim/$nb".sorted".alignreport.er | samtools view -bS - | samtools sort -T %s - -o $trim/$runname"_"$nb".primertrimmed.nonorm.sorted.bam"
+# samtools index $trim/$runname"_"$nb".primertrimmed.nonorm.sorted.bam"
+#
+# summary mapping stats
+# reads=$(samtools view $trim/$runname"_"$nb".sorted.bam" | cut -f 1 | sort | uniq | wc -l)
+# mapped=$(samtools view -F 4 $trim/$runname"_"$nb".sorted.bam" | cut -f 1 | sort | uniq | wc -l)
+# basesCovered=$(samtools depth $trim/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>0)' |wc -l)
+# basesCoveredx20=$(samtools depth $trim/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>=20)' |wc -l)
+# basesCoveredx100=$(samtools depth $trim/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>=100)' |wc -l)
+# echo -e $runname"_"${nb}"\t"$runtime"\t"$reads"\t"$mapped"\t"$basesCovered"\t"$basesCoveredx20"\t"$basesCoveredx100 >>$runname"_nb_mappingstats".txt
 
-#run preliminary mapping step to produce new reference scheme
-~/github/artic-rabv/other-scripts/rabies_prelimMap.sh $ref $pathToFastq $runname $nb
+#trim primers from fastq file for racon
+python ~/github/realtime-rabies/rampart/pipelines/process_sample/rules/trim_primers.py --reads $f --output_reads $trim/$runname"_"$nb.clipped.fastq
 
-#produce bam file for primer trimming step
-minimap2 -ax map-ont $newscheme/$runname"_prelim".reference.fasta $f | samtools view -u -| samtools sort - -T temp -o $trim/$runname"_"$nb".sorted.bam"
-
-#primer trimming
-align_trim  --normalise 200 ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $trim/$runname"_"$nb".sorted".alignreport.txt < $trim/$runname"_"$nb".sorted.bam" 2> $trim/$nb".sorted".alignreport.er | samtools view -bS - | samtools sort -T %s - -o $trim/$runname"_"$nb".primertrimmed.sorted.bam"
-samtools index $trim/$runname"_"$nb".primertrimmed.sorted.bam"
-align_trim  ~/github/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $trim/$runname"_"$nb".sorted".alignreport.txt < $trim/$runname"_"$nb".sorted.bam" 2> $trim/$nb".sorted".alignreport.er | samtools view -bS - | samtools sort -T %s - -o $trim/$runname"_"$nb".primertrimmed.nonorm.sorted.bam"
-samtools index $trim/$runname"_"$nb".primertrimmed.nonorm.sorted.bam"
-
-#convert trimmed bam file to fastq for medaka step
-samtools bam2fq $trim/$runname"_"$nb".primertrimmed.sorted.bam" > $trim/$runname"_"$nb".primertrimmed.sorted".fastq
-samtools bam2fq $trim/$runname"_"$nb".primertrimmed.nonorm.sorted.bam" > $trim/$runname"_"$nb".primertrimmed.norm.sorted".fastq
-
-#produce consensus in medaka
-#medaka_consensus -i $newscheme/$nb".primertrimmed.sorted".fastq -d $newscheme/$nb"_racon4".fasta -o $newscheme/$nb"_medaka" -t 2 -m r941_min_fast_g303
-#end=`date +%s`
-#runtime=$((end-start))
-
-#summary mapping stats
-reads=$(samtools view $trim/$runname"_"$nb".sorted.bam" | cut -f 1 | sort | uniq | wc -l)
-mapped=$(samtools view -F 4 $trim/$runname"_"$nb".sorted.bam" | cut -f 1 | sort | uniq | wc -l)
-basesCovered=$(samtools depth $trim/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>0)' |wc -l)
-basesCoveredx20=$(samtools depth $trim/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>=20)' |wc -l)
-basesCoveredx100=$(samtools depth $trim/$runname"_"$nb".primertrimmed.sorted.bam" | awk '($3>=100)' |wc -l)
-echo -e $runname"_"${nb}"\t"$runtime"\t"$reads"\t"$mapped"\t"$basesCovered"\t"$basesCoveredx20"\t"$basesCoveredx100 >>$runname"_nb_mappingstats".txt
-
-  done
-mv $runname"_nb_mappingstats".txt $trim/.
+done
+# mv $runname"_nb_mappingstats".txt $trim/.
--- other-scripts/racon_polish.sh
+++ other-scripts/racon_polish.sh
@@ -4,49 +4,44 @@ set -e
 
 ref=$1
 runname=$2
-bc=$3
+nb=$3
 
 #make directories for storage only if they don't exist already
-mkdir -p ~/pipeline_output/racon_polish/$runname"_racon"
-output=$(echo ~/pipeline_output/racon_polish/$runname"_racon")
+mkdir -p ~/pipeline_output/$runname/racon_polish/$nb
+output=$(echo ~/pipeline_output/$runname/racon_polish/$nb)
+trimmed=$(echo ~/pipeline_output/$runname/initial_mapTrim/$nb)
+newscheme=$(echo ~/pipeline_output/primer-schemes/$runname"_prelim"/$nb)
 
-minimap2 -x map-ont ~/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta $runname"_all-"$bc.fastq > $output/$bc"_map0.paf"
+#trim primers
+python '~/github/realtime-rabies/rampart/pipelines/process_sample/rules/trim_primers.py' --reads '/media/kirstyn/Peru1_kb/aqp1_gersa_all-NB01.fastq'  --output_reads test.fastq
 
-racon -m 8 -x -6 -g -8 -w 500 $runname"_all-"$bc.fastq $output/$bc"_map0.paf" ~/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta > $output/$bc"_racon0.fasta"
+minimap2 -x map-ont $newscheme/$runname"_prelim".reference.fasta $trimmed/$runname"_"$nb".primertrimmed.nonorm.sorted.fastq" > $output/$nb"_map0.paf"
 
-#racon -t 1 $runname"_all-"$bc.fastq $output/$bc"_map0.paf" ~/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta > $output/$bc"_racon0.fasta"
+racon -m 8 -x -6 -g -8 -w 500 $trimmed/$runname"_"$nb".primertrimmed.nonorm.sorted.fastq" $output/$nb"_map0.paf" $newscheme/$runname"_prelim".reference.fasta > $output/$nb"_racon0.fasta"
+
+#racon -t 1 $runname"_all-"$nb.fastq $output/$nb"_map0.paf" ~/artic-rabv/primer-schemes/$ref/V1/$ref.reference.fasta > $output/$nb"_racon0.fasta"
 
 #racon 4x polish
 for i in {1..4}
 do
-minimap2 -x map-ont $output/$bc"_racon"$[i-1]".fasta" $runname"_all-"$bc.fastq > $output/$bc"_map"$i".paf"
+minimap2 -x map-ont $output/$nb"_racon"$[i-1]".fasta" $trimmed/$runname"_"$nb".primertrimmed.norm.sorted.fastq" > $output/$nb"_map"$i".paf"
 
-racon -m 8 -x -6 -g -8 -w 500  $runname"_all-"$bc.fastq $output/$bc"_map"$i".paf" $output/$bc"_racon"$[i-1]".fasta" > $output/$bc"_racon"$i".fasta"
+racon -m 8 -x -6 -g -8 -w 500  $trimmed/$runname"_"$nb".primertrimmed.norm.sorted.fastq" $output/$nb"_map"$i".paf" $output/$nb"_racon"$[i-1]".fasta" > $output/$nb"_racon"$i".fasta"
 
-#racon -t 1 $runname"_all-"$bc.fastq $output/$bc"_map"$i".paf" $output/$bc"_racon"$[i-1]".fasta" > $output/$bc"_racon"$i".fasta"
+#racon -t 1 $runname"_all-"$nb.fastq $output/$nb"_map"$i".paf" $output/$nb"_racon"$[i-1]".fasta" > $output/$nb"_racon"$i".fasta"
 
 done
 
-#produce bam file for primer trimming step
-#minimap2 -ax map-ont $output/$bc"_racon4".fasta $runname"_all-"$bc.fastq | samtools view -u -| samtools sort - -T temp -o $output/$bc".sorted.bam"
-
-#primer trimming
-#align_trim  --normalise 200 ~/artic-rabv/primer-schemes/$ref/V1/$ref.scheme.bed --report $output/$bc".sorted".alignreport.txt < $output/$bc".sorted.bam" 2> $output/$bc".sorted".alignreport.er | samtools view -bS - | samtools sort -T %s - -o $output/$bc".primertrimmed.sorted.bam" 
-#samtools index $output/$bc".primertrimmed.sorted.bam" 
-
-#convert trimmed bam file to fastq for medaka step
-#samtools bam2fq $output/$bc".primertrimmed.sorted.bam" > $output/$bc".primertrimmed.sorted".fastq
-
 #produce consensus in medaka
-#medaka_consensus -i $output/$bc".primertrimmed.sorted".fastq -d $output/$bc"_racon4".fasta -o $output/$bc"_medaka" -t 2 -m r941_min_fast_g303
+#medaka_consensus -i $output/$nb".primertrimmed.sorted".fastq -d $output/$nb"_racon4".fasta -o $output/$nb"_medaka" -t 2 -m r941_min_fast_g303
 
-medaka_consensus -i $runname"_all-"$bc.fastq -d $output/$bc"_racon4".fasta -o $output/$bc"_medaka" -t 2 -m r941_min_fast_g303
+medaka_consensus -i $trimmed/$runname"_"$nb".primertrimmed.norm.sorted.fastq" -d $output/$nb"_racon4".fasta -o $output/$nb"_medaka2" -t 2 -m r941_min_fast_g303
 
 #fill in gaps if consensus is in segments
 #get positions of missing regions
-cd $output/$bc"_medaka"
+cd $output/$nb"_medaka2"
 grep "^>" consensus.fasta | sed 's/[>:]/\t/g'| cut -f 3 | sed 's/[-]/\t/g' >tmp.txt
-sed -i 's/[.0]//g' tmp.txt 
+sed -i 's/[.0]//g' tmp.txt
 if [[ $(wc -l <tmp.txt) -ge 1 ]]
 then
 echo "Consensus has gaps"
@@ -64,10 +59,5 @@ done
 tr '\n' 'x' <consensus.fasta  | sed 's/x//2g' > consensus2.fasta
 sed -i 's/x/\n/g' consensus2.fasta
 rm -rf consensus.fasta
-mv consensus2.fasta consensus.fasta 
+mv consensus2.fasta consensus.fasta
 fi
-
-
-
-
-
