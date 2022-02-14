687b69d074122d3a9711f92377c4ddf241d1a3cb
kirstyn <kirstyn.brunker@glasgow.ac.uk>
2022-02-14 07:55:32 +0000

    Update run_to_fastq.sh

--- NEW/run_to_fastq.sh
+++ NEW/run_to_fastq.sh
@@ -1,7 +1,7 @@
 #!   /bin/bash
 usage()
 {
-    echo "usage: <command> run_name path_to_fast5"
+    echo "usage: <command> run_name path_to_fast5 barcode_kit primer_scheme"
 }
 if [ -z "$*" ]
 then
@@ -12,6 +12,8 @@ fi
 # set up analysis folders
 run_name=$1
 path_to_reads=$2
+barcodes=$3
+scheme=$4
 #bc_set=( $(grep -o "barcode.." $bc_config | grep "barcode") )
 
 mkdir -p analysis
@@ -20,11 +22,12 @@ cp barcodes.csv analysis/$run_name/.
 cd analysis/$run_name
 bc_config=$(find . -name barcodes.csv)
 #fast basecalling
-guppy_basecaller -c dna_r9.4.1_450bps_fast.cfg -i $path_to_reads -s $run_name"_guppy" -r
-#-x auto
+guppy_basecaller -c dna_r9.4.1_450bps_fast.cfg -i $path_to_reads -s $run_name"_guppy" -r -x auto
 
 #demultiplex
-guppy_barcoder -r --require_barcodes_both_ends -i $run_name"_guppy" -s $run_name"_demux" --arrangements_files "barcode_arrs_nb12.cfg barcode_arrs_nb24.cfg"
+guppy_barcoder -r --require_barcodes_both_ends -i $run_name"_guppy" -s $run_name"_demux" --barcode_kits "${barcodes}"
+# guppy pre v6.0
+#guppy_barcoder -r --require_barcodes_both_ends -i $run_name"_guppy" -s $run_name"_demux" --arrangements_files "barcode_arrs_nb12.cfg barcode_arrs_nb24.cfg"
 
 #concat reads and filter on read length
 mkdir -p $run_name"_filtered"
@@ -45,7 +48,7 @@ continue
 else
 mkdir -p $sample
 #rename files
-artic minion --medaka --normalise 200 --threads 4 --scheme-directory ~/Github/artic-rabv/primer-schemes --read-file $f rabvTanzDg/V2 $sample/$sample
+artic minion --medaka --normalise 200 --threads 4 --scheme-directory ~/Github/artic-rabv/primer-schemes --read-file $f ${primer_scheme} $sample/$sample
 
 #mv $f $run_name"_filtered"/$(basename $f | sed -e "s/$bc/$sample/")
 fi
