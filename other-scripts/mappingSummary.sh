ef7e1db84d16fd0a01268f14e3edc238c8b4fba6
“ctbautista” <8essel8@gmail.com>
2023-01-18 01:59:29 +0000

    Update mappingSummary.sh

--- other-scripts/mappingSummary.sh
+++ other-scripts/mappingSummary.sh
@@ -7,14 +7,22 @@ for bam in $(find . -name "*.sorted.bam" ! -name "*trimmed*")
 do
 dirname=$(dirname $bam)
 path=$(realpath $bam | sed 's/ //g')
-runname=$(echo ${dirname} | sed -e 's/NB..//' -e 's/.*PER_*//g' -e 's/.*TZA_*//g' -e 's/.*PHL_*//g' -e 's/.*KEN_*//g')
+runname=$(echo ${dirname} | sed -e 's/NB..//' -e 's/\///g' -e 's/\.//g')
 if [ -z ${runname} ]
 then
 runname=$(builtin cd ${dirname}; pwd | sed 's/.*\///g')
 fi
+echo ${runname}
 stub=$(basename $bam)
 stub=${stub%*.sorted.bam}
 mkdir -p ${PWD##*/}_depthFiles
+
+# depth of cov per base
+if [ -f ${PWD##*/}_depthFiles/${stub}_${runname}_depth.txt ]
+then
+    continue
+fi
+samtools depth -a $bam -d 500000 > ${PWD##*/}_depthFiles/${stub}_${runname}_depth.txt
 
 # summary of mapped reads
 reads=$(samtools view $bam | cut -f 1 | sort | uniq | wc -l)
@@ -25,15 +33,8 @@ basesCoveredx5=$(samtools depth $bam | awk '($3>=5)' |wc -l)
 basesCoveredx20=$(samtools depth $bam | awk '($3>=20)' |wc -l)
 basesCoveredx100=$(samtools depth $bam | awk '($3>=100)' |wc -l)
 echo -e $runname $stub $reads $mapped $mean $basesCovered $basesCoveredx5 $basesCoveredx20 $basesCoveredx100 $path>> temp.txt
-
-# depth of cov per base
-if [ -f ${PWD##*/}_depthFiles/${stub}_${runname}_depth.txt ]
-then
-    continue
-fi
-samtools depth -a $bam -d 500000 > ${PWD##*/}_depthFiles/${stub}_${runname}_depth.txt
 done
 
 { printf 'runname sample_id total_reads mapped_reads meanReads sd_reads median_reads min_reads max_reads basesCovered_1 basesCovered_5 basesCovered_20 basesCovered_100 filepath\n'; cat temp.txt; }> ${runname}_mappingStats.txt
-sed -i 's/ /\t/g' ${runname}_mappingStats.txt
+gsed -i 's/ /\t/g' ${runname}_mappingStats.txt
 rm temp.txt
